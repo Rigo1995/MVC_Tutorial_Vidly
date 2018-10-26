61dbf10da431147fb132fe877954432118574480
rigo <rigobertoramirez606@gmail.com>
2018-10-25 23:11:28 -0500

    initial commit

---
+++ MVC_Tutorial_Vidly/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Tutorial_Vidly.Startup))]
+namespace MVC_Tutorial_Vidly
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
